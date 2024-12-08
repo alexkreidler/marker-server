4d2e69cb5549ca3cc6afbfc5adae1b937a3bfb49
Alex Kreidler <alex.contributing@alexkreidler.com>
2024-12-08 16:41:39 -0500

    add basic modal, update docker

---
+++ src/modal_server.py
@@ -0,0 +1,14 @@
+import modal
+from server import make_server
+app = modal.App("marker")
+
+@app.function(
+    image=modal.Image.from_registry("ghcr.io/alexkreidler/marker-server:1.0.2-modal"),
+    gpu=modal.gpu.A10G(count=1),
+    container_idle_timeout=20,
+    timeout=24 * 60 * 60,
+    allow_concurrent_inputs=1000,
+)
+@modal.asgi_app()
+def serve():
+    return make_server()
